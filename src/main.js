f720a9c93cf9d104c7e28947e1aae5ff3118fd9e
Mr Damn Coder <luantranphu901@gmail.com>
2025-03-27 10:36:31 +0700

    remove Font Awesome CSS import from main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import i18n from "./i18n";
-import "@fortawesome/fontawesome-free/css/all.css";
 
 const app = createApp(App);
 
